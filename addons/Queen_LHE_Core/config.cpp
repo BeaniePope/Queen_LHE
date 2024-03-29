8c7f53a2ddaf2f24d6f4744e7c1bced1aa6ab9b2
BeaniePope <Swifter11110@gmail.com>
2024-03-28 23:13:00 -0400

    first commit

---
+++ addons/Queen_LHE_Core/config.cpp
@@ -0,0 +1,42 @@
+class CfgPatches
+{
+    class Queen_LHE_Core
+    {
+        name = "Queen LHE Core";
+        author = "Queen";
+        requiredAddons[]=
+        {
+            "fallout_uniform_03"
+        };
+        units[]=
+        {
+            "BOSLHE_Field_Scribe_Uniform",
+            "BOSLHE_Field_Scribe_Uniform_wBackpack",
+            "BOSLHE_Medical_Scribe_Uniform",
+            "BOSLHE_Medical_Scribe_Uniform_wBackpack",
+            "BOSLHE_Engineer_Scribe_Uniform_",
+            "BOSLHE_Engineer_Scribe_Uniform_wBackpack",
+            "BOSLHE_Lancer_Captain",
+            "BOSLHE_Combat_Armor_Black_Black",
+            "BOSLHE_Combat_Armor_Black_Blue",
+            "BOSLHE_Combat_Armor_Black_Desert",
+            "BOSLHE_Combat_Armor_Black_Green",
+            "BOSLHE_Combat_Armor_Black_White",
+            "BOSLHE_Combat_Armor_Olive_Black",
+            "BOSLHE_Combat_Armor_Olive_Blue",
+            "BOSLHE_Combat_Armor_Olive_Desert",
+            "BOSLHE_Combat_Armor_Olive_Green",
+            "BOSLHE_Combat_Armor_Olive_White",
+            "BOSLHE_Combat_Armor_Orange_Black",
+            "BOSLHE_Combat_Armor_Orange_Blue",
+            "BOSLHE_Combat_Armor_Orange_Desert",
+            "BOSLHE_Combat_Armor_Orange_Green",
+            "BOSLHE_Combat_Armor_Orange_White",
+        }
+        weapons[]
+        {
+            ""
+        }
+
+    };
+};
