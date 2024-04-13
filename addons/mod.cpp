407b6b9cd27689df25f99089078c9a7df356dccf
BeaniePope <Swifter11110@gmail.com>
2024-04-13 18:01:44 -0400

    modcpp and markers

---
+++ addons/mod.cpp
@@ -0,0 +1,13 @@
+name = "Brotherhood of Steel Auxiliary";
+author = "BOS Mod Team";
+logo = "\addons\LHE_Core\data\ui\markers\BOSSPMarker.paa";
+logoOver = "\addons\LHE_Core\data\ui\markers\BOSSPMarker.paa";
+tooltip = "BOS Aux";
+tooltipOwned = "Written by BOS Mod Team";
+actionName = "Github";
+action = "https://github.com/BeaniePope/Queen_LHE"; 
+overview = "Auxiliary mod for a Brotherhood of Steel unit.";
+hideName = 0;
+hidePicture = 0;
+dlcColor[] = {0, 0, 0, 0.6};
+logoSmall = "\addons\LHE_Core\data\ui\markers\BOSSPMarker.paa";
